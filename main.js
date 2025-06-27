030ed747f1e496766e8efe1f54afe3e67fef657e
MathomJohnson <156151500+MathomJohnson@users.noreply.github.com>
2025-06-26 21:39:32 -0500

    asdf

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ initRoadLines(canvas.height);
 // Define constants
 const SPEED = 1;
 const NUM_OBSTACLES = 10;
-const POPULATION_SIZE = 100;
+const POPULATION_SIZE = 50;
 const PARENT_COUNT = 10;
 const MUTATION_RATE = 0.2;
 
